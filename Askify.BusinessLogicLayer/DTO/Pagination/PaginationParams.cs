8e858b4b9aceb4b5608a8948ba632f0c250c791f
lesichk0 <olesiaproskurniak@gmail.com>
2025-05-18 07:26:52 +0300

    added pagination

---
+++ Askify.BusinessLogicLayer/DTO/Pagination/PaginationParams.cs
@@ -0,0 +1,15 @@
+﻿namespace Askify.BusinessLogicLayer.DTO.Pagination
+{
+    public class PaginationParams
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        public const int MaxPageSize = 50;
+
+        public int GetSafePageSize()
+        {
+            return PageSize > MaxPageSize ? MaxPageSize : PageSize;
+        }
+    }
+}
